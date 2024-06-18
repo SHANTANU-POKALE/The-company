e66e901ff800bdaa0667757f1533ca7a214ae5ca
shantanu pradip pokale <shantanupokale009@gmail.com>
2024-06-18 22:11:55 +0530

    day 23: Add pass to environment

--- send_email.py
+++ send_email.py
@@ -1,4 +1,5 @@
 import smtplib, ssl
+import os
 
 
 def send_email(message):
@@ -6,7 +7,7 @@ def send_email(message):
     port = 465
 
     username = "festivalphotos004@gmail.com"
-    password = "cmtw rvye zrto wueh"
+    password = os.getenv("PASSWORD")
 
     receiver = "festivalphotos004@gmail.com"
     context = ssl.create_default_context()
