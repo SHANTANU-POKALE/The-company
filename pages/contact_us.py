b2a54e350b1a3fc5e675878df7cd932288315b04
shantanu pradip pokale <shantanupokale009@gmail.com>
2024-06-18 22:02:34 +0530

    day 23: final commit company

--- pages/contact_us.py
+++ pages/contact_us.py
@@ -1,15 +1,21 @@
 import streamlit as st
 from send_email import send_email
+import pandas
 
-st.header("Contact Me")
+df = pandas.read_csv("topics.csv")
 
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email address")
+    option = st.selectbox(
+        "What topics do you want to discuss?",
+        df["topic"])
+
     raw_message = st.text_area("Your message")
     message = f"""\
 Subject: New email from {user_email}
 
 From: {user_email}
+Topic {option}
 {raw_message}
 """
     button = st.form_submit_button()
